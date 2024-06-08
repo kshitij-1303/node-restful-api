dc05f3aacc8fb22c9d4caf57af5b1b5cca6d1e9e
Kshitij Deshpande <kshitijdeshpande9325@gmail.com>
2024-06-08 12:00:51 +0530

    Restful api implemented

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
+const port = 3001;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "kshitij",
+    content: "I am learning about REST",
+  },
+  {
+    id: uuidv4(),
+    username: "suyash",
+    content: "I live in nagpur",
+  },
+  {
+    id: uuidv4(),
+    username: "apnacollege",
+    content: "This is delta batch",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  console.log(req.body);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`app is listening to port ${port}`);
+});
